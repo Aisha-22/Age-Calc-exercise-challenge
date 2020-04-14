c5b5365b2e56bd5863cf14e807db8c6e2144cf33
Aisha <ash.hlatshwayo22@gmail.com>
2020-04-14 15:42:47 +0200

    Simple js revision with Colt

---
+++ agecalc.js
@@ -0,0 +1,8 @@
+var age = prompt("What is your age?");
+var days = age * 365;
+
+// Colts correct solution below:
+alert(age + " years is roughly " + days + " days");
+
+// My solution below:
+console.log("My age is " + age + " which is roughly " + days + " days");
